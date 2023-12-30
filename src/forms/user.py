3de5bc33c29361ea2bd874935452818062ce20fb
hasansezertasan <hasansezertasan@gmail.com>
2023-12-30 17:45:12 +0300

    Added some properties and formatted.

--- src/forms/user.py
+++ src/forms/user.py
@@ -1,33 +1,88 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SelectField, StringField, SubmitField
+from wtforms import EmailField, PasswordField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class RegisterForm(FlaskForm):
-    username = StringField("Kullanıcı Adı", validators=[DataRequired()])
-    password = PasswordField("Parola", validators=[DataRequired()])
-    password_again = PasswordField("Parola Tekrar", validators=[DataRequired(), EqualTo("password")])
-    name = StringField("Ad", validators=[DataRequired()])
-    surname = StringField("Soyad", validators=[DataRequired()])
-    email = StringField("E-posta", validators=[DataRequired(), Email()])
-    submit = SubmitField("Kayıt Ol")
+    username = StringField(
+        label="Kullanıcı Adı",
+        validators=[DataRequired()],
+    )
+    password = PasswordField(
+        label="Parola",
+        validators=[DataRequired()],
+    )
+    password_again = PasswordField(
+        label="Parola Tekrar",
+        validators=[DataRequired(), EqualTo("password")],
+    )
+    name = StringField(
+        label="Ad",
+        validators=[DataRequired()],
+    )
+    surname = StringField(
+        label="Soyad",
+        validators=[DataRequired()],
+    )
+    email = StringField(
+        label="E-posta",
+        validators=[DataRequired(), Email()],
+    )
+    submit = SubmitField(
+        label="Kayıt Ol",
+        description="Kayıt olmak için tıklayın.",
+    )
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Kullanıcı Adı", validators=[DataRequired()])
-    password = PasswordField("Parola", validators=[DataRequired()])
-    submit = SubmitField("Giriş Yap")
+    username = StringField(
+        label="Kullanıcı Adı",
+        validators=[DataRequired()],
+        description="Kullanıcı adınızı girin.",
+    )
+    password = PasswordField(
+        label="Parola",
+        validators=[DataRequired()],
+    )
+    submit = SubmitField(
+        label="Giriş Yap",
+        description="Giriş yapmak için tıklayın.",
+    )
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField("Parola", validators=[DataRequired()])
-    new_password = PasswordField("Yeni Parola", validators=[DataRequired()])
-    new_password_again = PasswordField("Yeni Parola Tekrar", validators=[DataRequired(), EqualTo("new_password")])
-    submit = SubmitField("Şifreyi Değiştir")
+    password = PasswordField(
+        label="Parola",
+        validators=[DataRequired()],
+    )
+    new_password = PasswordField(
+        label="Yeni Parola",
+        validators=[DataRequired()],
+    )
+    new_password_again = PasswordField(
+        label="Yeni Parola Tekrar",
+        validators=[DataRequired(), EqualTo("new_password")],
+    )
+    submit = SubmitField(
+        label="Şifreyi Değiştir",
+        description="Şifrenizi değiştirmek için tıklayın.",
+    )
 
 
 class EditProfileForm(FlaskForm):
-    name = StringField("Ad", validators=[DataRequired()])
-    surname = StringField("Soyad", validators=[DataRequired()])
-    email = StringField("E-posta", validators=[DataRequired(), Email()])
-    submit = SubmitField("Değişiklikleri Kaydet")
+    name = StringField(
+        label="Ad",
+        validators=[DataRequired()],
+    )
+    surname = StringField(
+        label="Soyad",
+        validators=[DataRequired()],
+    )
+    email = StringField(
+        label="E-posta",
+        validators=[DataRequired(), Email()],
+    )
+    submit = SubmitField(
+        label="Değişiklikleri Kaydet",
+        description="Profil bilgilerini güncellemek için tıklayın.",
+    )
